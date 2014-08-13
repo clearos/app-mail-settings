0a413c2dff06b177cf131af6b1cb600e621f50bf
pcbaldwin <pbaldwin@clearfoundation.com>
2014-08-13 01:00:30 -0400

    Added deprecated notice

--- language/en_US/mail_settings_lang.php
+++ language/en_US/mail_settings_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['mail_settings_app_name'] = 'Mail Settings';
+$lang['mail_settings_app_name'] = 'Mail Settings (deprecated)';
 $lang['mail_settings_app_description'] = 'The Mail Settings app provides the necessary tools to manage core mail functionality.';
 $lang['mail_settings_network_settings'] = 'Network Settings';
 $lang['mail_settings_mail_domain'] = 'Mail Domain';
