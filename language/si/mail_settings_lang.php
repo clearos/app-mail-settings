4ea69f2610d3b161b83e662242b4471ea219dd9d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:07:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'මේල් සැකසුම්';
+$lang['mail_settings_app_description'] = 'ඇති තැපැල් සැකසුම් යෙදුම මූලික තැපැල ක්රියාකාරිත්වය කළමනාකරණය සඳහා අවශ්ය මෙවලම් සපයයි.';
+$lang['mail_settings_network_settings'] = 'ජාල පසුතල';
+$lang['mail_settings_mail_domain'] = 'තැපැල් වසම්';
