8a84bf6426710bb8a74ec1675e883d5790c34ce9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:59:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Configuració de Missatges';
+$lang['mail_settings_app_description'] = 'L&#39;aplicació de configuració de Missatges proporciona les eines necessàries per gestionar la funcionalitat de correu nucli.';
+$lang['mail_settings_network_settings'] = 'Configuració de la xarxa';
+$lang['mail_settings_mail_domain'] = 'domini de correu';
