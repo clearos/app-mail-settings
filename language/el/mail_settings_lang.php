45a8c9b8a0fa9f00fd9fb20edd803749f353eeda
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:01:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Ρυθμίσεις Mail';
+$lang['mail_settings_app_description'] = 'Η εφαρμογή Ρυθμίσεις Mail παρέχει τα απαραίτητα εργαλεία για τη διαχείριση βασική λειτουργικότητα ταχυδρομείου.';
+$lang['mail_settings_network_settings'] = 'Ρυθμίσεις δικτύου';
+$lang['mail_settings_mail_domain'] = 'Mail τομέα';
