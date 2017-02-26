2f3ab0c4b4779b74df19dc0c75426cafbe25adba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:58:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Settings Mail';
+$lang['mail_settings_app_description'] = 'Aplikacioni Mail Settings siguron mjetet e nevojshme për të menaxhuar funksionet kryesore të postës.';
+$lang['mail_settings_network_settings'] = 'Cilësimet e rrjetit';
+$lang['mail_settings_mail_domain'] = 'Mail Domain';
