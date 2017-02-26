dd8f8a57289b94a61daeee7301ea906afddfd050
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:02:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'mail Settings';
+$lang['mail_settings_app_description'] = 'The Mail Settings ngwa na-enye ndị dị mkpa ngwá ọrụ iji jikwaa isi mail arụmọrụ.';
+$lang['mail_settings_network_settings'] = 'Network Ntọala';
+$lang['mail_settings_mail_domain'] = 'mail Domain';
