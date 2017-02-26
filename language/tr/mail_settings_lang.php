777d67f0e5b7c9bf72b970410ea34be66acd7913
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:09:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'posta Ayarları';
+$lang['mail_settings_app_description'] = 'Posta Ayarları uygulaması çekirdek posta işlevselliği yönetmek için gerekli araçları sağlar.';
+$lang['mail_settings_network_settings'] = 'Ağ ayarları';
+$lang['mail_settings_mail_domain'] = 'posta Etki Alanı';
