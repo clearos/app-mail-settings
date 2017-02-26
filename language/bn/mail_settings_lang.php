82dbb8adef13f99b38f20213d4d6a2a5f97854b8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:59:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'মেল সেটিংস';
+$lang['mail_settings_app_description'] = 'মেল সেটিংস অ্যাপ্লিকেশন কোর মেইল ​​কার্যকারিতা পরিচালনা করার জন্য প্রয়োজনীয় সামগ্রী উপলব্ধ করা.';
+$lang['mail_settings_network_settings'] = 'নেটওয়ার্ক সেটিংস';
+$lang['mail_settings_mail_domain'] = 'মেল সহায়তা ডোমেন';
