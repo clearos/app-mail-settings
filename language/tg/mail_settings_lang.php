f76f04ed7acd6ae3f5d362eeb3251570889d5188
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:08:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Танзимот Mail';
+$lang['mail_settings_app_description'] = 'Дар барнома танзимоти почтаи электронӣ таъмин асбобҳои зарурӣ идора ва имконоти коркарди почтаи асосии.';
+$lang['mail_settings_network_settings'] = 'Танзимоти шабака';
+$lang['mail_settings_mail_domain'] = 'Mail Domain';
