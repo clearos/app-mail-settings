383ff1524f0d6f271c479263f353a889c757184a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:09:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'میل کی ترتیبات';
+$lang['mail_settings_app_description'] = 'میل کی ترتیبات کو اے پی پی کور میل فعالیت کا انتظام کرنے کے لئے ضروری آلات فراہم کرتا ہے.';
+$lang['mail_settings_network_settings'] = 'نیٹ ورک کی ترتیبات';
+$lang['mail_settings_mail_domain'] = 'میل ڈومین';
