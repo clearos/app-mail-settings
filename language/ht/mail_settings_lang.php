04e7e9f2453533146bd655d87b98ca363e83ce64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:02:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'mail Anviwònman';
+$lang['mail_settings_app_description'] = 'App a Mail Anviwònman bay zouti ki nesesè nan jere fonctionnalités mail prensipal yo.';
+$lang['mail_settings_network_settings'] = 'Rezo Anviwònman';
+$lang['mail_settings_mail_domain'] = 'mail Domèn';
