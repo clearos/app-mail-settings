576ec140af15964b0b9fb9a6dce0eecd806afe81
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:01:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Paramètres de messagerie';
+$lang['mail_settings_app_description'] = 'L&#39;application Mail Settings fournit les outils nécessaires pour gérer les fonctionnalités de messagerie de base.';
+$lang['mail_settings_network_settings'] = 'Paramètres réseau';
+$lang['mail_settings_mail_domain'] = 'domaine de messagerie';
