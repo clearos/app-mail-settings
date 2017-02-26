3729fba64144de8ee3bc089856be77b5a831fec5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:03:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'ការកំណត់សំបុត្រ';
+$lang['mail_settings_app_description'] = 'ការកំណត់សំបុត្រកម្មវិធីផ្តល់នូវឧបករណ៍ចាំបាច់ដើម្បីគ្រប់គ្រងមុខងារសំបុត្រស្នូល។';
+$lang['mail_settings_network_settings'] = 'ការកំណត់​ប​ណ្តា​ញ';
+$lang['mail_settings_mail_domain'] = 'ដែនសំបុត្រ';
