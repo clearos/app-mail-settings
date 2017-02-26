314355c315e3392dac5c12dcecd6cd6a848a9a51
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:03:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'ფოსტის პარამეტრები';
+$lang['mail_settings_app_description'] = 'ფოსტის პარამეტრები app უზრუნველყოფს საჭირო ინსტრუმენტები მართოთ ძირითადი mail ფუნქცია.';
+$lang['mail_settings_network_settings'] = 'ქსელის პარამეტრები';
+$lang['mail_settings_mail_domain'] = 'ფოსტა Domain';
