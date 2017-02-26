d2b24a5172bed8c9ee3e708c89b1d30521790678
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:08:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'การตั้งค่าอีเมล';
+$lang['mail_settings_app_description'] = 'แอปตั้งค่าจดหมายมีเครื่องมือที่จำเป็นในการจัดการการทำงานอีเมลหลัก';
+$lang['mail_settings_network_settings'] = 'ตั้งค่าเครือข่าย';
+$lang['mail_settings_mail_domain'] = 'โดเมนจดหมาย';
