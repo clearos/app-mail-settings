98094555854de79525d571655f0bb67e8f30967d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:02:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Mail Socruithe';
+$lang['mail_settings_app_description'] = 'Soláthraíonn an app Socruithe Mail na huirlisí is gá chun feidhmiúlacht ríomhphoist lárnach bhainistiú.';
+$lang['mail_settings_network_settings'] = 'Socruithe líonra';
+$lang['mail_settings_mail_domain'] = 'Mail Fearann';
