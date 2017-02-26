e672ad7475bc43d504d9b120bd6905228607ea33
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:04:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Mail Settings';
+$lang['mail_settings_app_description'] = 'Ny fampiharana Settings Mail manome ny fitaovana ilaina amin&#39;ny fitantanana fototra namany Sary miasa.';
+$lang['mail_settings_network_settings'] = 'Network Settings';
+$lang['mail_settings_mail_domain'] = 'Mail Domain';
