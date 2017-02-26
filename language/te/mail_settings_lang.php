3cf7cbec8195f1ba68e3db78fe555ee38550067b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:08:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'మెయిల్ సెట్టింగ్లు';
+$lang['mail_settings_app_description'] = 'మెయిల్ సెట్టింగ్ల అనువర్తనాన్ని కోర్ మెయిల్ కార్యాచరణను నిర్వహించడానికి అవసరమైన సాధనాలను అందించును.';
+$lang['mail_settings_network_settings'] = 'నెట్వర్క్ అమరికలు';
+$lang['mail_settings_mail_domain'] = 'మెయిల్ డొమైన్';
