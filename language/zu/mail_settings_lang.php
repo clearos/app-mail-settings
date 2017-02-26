c3ea88a67d8ec4bc729595dba6d2a17422d42764
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:10:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Izilungiselelo Mail';
+$lang['mail_settings_app_description'] = 'Insiza izilungiselelo Mail inikeza amathuluzi adingekayo ukuphatha core imeyili ukusebenza.';
+$lang['mail_settings_network_settings'] = 'Izilungiselelo Network';
+$lang['mail_settings_mail_domain'] = 'Mail Domain';
