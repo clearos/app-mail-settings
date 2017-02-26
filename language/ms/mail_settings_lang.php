5f9ddcd9d97079b5d81578a11fc97ba0f0f68f96
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:05:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Tetapan mail';
+$lang['mail_settings_app_description'] = 'Aplikasi Mel Tetapan menyediakan alat yang diperlukan untuk menguruskan fungsi mail teras.';
+$lang['mail_settings_network_settings'] = 'Tetapan rangkaian';
+$lang['mail_settings_mail_domain'] = 'mail Domain';
