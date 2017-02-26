94d5bc5de42a2992b0143d0b6fe69dee1b1b2a94
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:02:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Mail Saituna';
+$lang['mail_settings_app_description'] = 'The Mail Saituna app bayar da zama dole kayayyakin aiki, don sarrafa core mail ayyuka.';
+$lang['mail_settings_network_settings'] = 'Network Saituna';
+$lang['mail_settings_mail_domain'] = 'Mail Domain';
