99fd6a3f730753d7027b0176b93651d25a23c0b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:09:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Cài đặt email';
+$lang['mail_settings_app_description'] = 'Các ứng dụng Mail Settings cung cấp những công cụ cần thiết để quản lý mail chức năng cốt lõi.';
+$lang['mail_settings_network_settings'] = 'Thiết lạp mạng lưới';
+$lang['mail_settings_mail_domain'] = 'Tên miền thư';
