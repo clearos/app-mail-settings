0ef8de78845a3f073cf7d4b2fbb69d83e4ab8784
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:02:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'mail beállítások';
+$lang['mail_settings_app_description'] = 'A Mail Settings app biztosítja a szükséges eszközöket kezelni core mail funkciókat.';
+$lang['mail_settings_network_settings'] = 'Hálózati beállítások';
+$lang['mail_settings_mail_domain'] = 'mail Domain';
