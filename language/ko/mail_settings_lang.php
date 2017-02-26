01c1e28624018fc4e70412b60a535b9590d677de
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:03:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = '메일 설정';
+$lang['mail_settings_app_description'] = '메일 설정 응용 프로그램은 핵심 메일 기능을 관리하는 데 필요한 도구를 제공합니다.';
+$lang['mail_settings_network_settings'] = '네트워크 설정';
+$lang['mail_settings_mail_domain'] = '메일 도메인';
