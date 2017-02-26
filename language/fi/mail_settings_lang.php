2b3451f6c32e2ba78408a33f04a440cd8b0aa9e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:01:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Postiasetukset';
+$lang['mail_settings_app_description'] = 'Mail Asetukset-sovellus tarjoaa tarvittavat välineet hallintaan ydin postia toiminnallisuutta.';
+$lang['mail_settings_network_settings'] = 'Verkkoasetukset';
+$lang['mail_settings_mail_domain'] = 'Domain';
