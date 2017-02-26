6fc9b8fe41724f4235c91855647d91773403efcb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:06:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Setări e-mail';
+$lang['mail_settings_app_description'] = 'Aplicația Setări e-mail oferă instrumentele necesare pentru a gestiona funcționalitatea de bază e-mail.';
+$lang['mail_settings_network_settings'] = 'Setari de retea';
+$lang['mail_settings_mail_domain'] = 'Domeniu e-mail';
