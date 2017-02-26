872b934d138a911b02c514bd2700f65562227ddf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:58:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Mail Settings';
+$lang['mail_settings_app_description'] = 'Die app Mail Settings verskaf die nodige gereedskap om kern pos funksies te bestuur.';
+$lang['mail_settings_network_settings'] = 'netwerk Stellings';
+$lang['mail_settings_mail_domain'] = 'pos Domain';
