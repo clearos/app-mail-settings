a5f9f2c4e5b7dedf7c282ac3b47823ab9a4861a3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:06:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Ustawienia poczty';
+$lang['mail_settings_app_description'] = 'Aplikacja Ustawienia poczty zapewnia niezbędne narzędzia do zarządzania podstawową funkcjonalność poczty.';
+$lang['mail_settings_network_settings'] = 'Ustawienia sieci';
+$lang['mail_settings_mail_domain'] = 'Poczta Domain';
