bac8d573d9e10d384eea5ea56ad4cd4f35a70d11
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:07:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'nastavenie pošty';
+$lang['mail_settings_app_description'] = 'Aplikácia Nastavenie pošty poskytuje potrebné nástroje pre správu základné funkcie pošty.';
+$lang['mail_settings_network_settings'] = 'Nastavenia siete';
+$lang['mail_settings_mail_domain'] = 'doména pošty';
