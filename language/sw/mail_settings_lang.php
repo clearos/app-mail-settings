be99180a742654cb1910701df4aed5845f5f541d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:08:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Mazingira mail';
+$lang['mail_settings_app_description'] = 'programu Mail Settings hutoa zana muhimu kusimamia msingi mail utendaji.';
+$lang['mail_settings_network_settings'] = 'Mazingira Network';
+$lang['mail_settings_mail_domain'] = 'Barua Domain';
