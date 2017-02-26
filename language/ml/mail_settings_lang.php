7ae4d86967992b78170c01891257bfeb85c0753d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:05:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'മെയിൽ ക്രമീകരണങ്ങൾ';
+$lang['mail_settings_app_description'] = 'മെയിൽ ക്രമീകരണങ്ങൾ അപ്ലിക്കേഷൻ കോർ മെയിൽ പ്രവർത്തനക്ഷമത നിയന്ത്രിക്കാൻ ആവശ്യമായ ഉപകരണങ്ങൾ നൽകുന്നു.';
+$lang['mail_settings_network_settings'] = 'നെറ്റ്വർക്ക് ക്രമീകരണങ്ങൾ';
+$lang['mail_settings_mail_domain'] = 'മെയിൽ ഡൊമെയ്ൻ';
