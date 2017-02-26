dbb02c918f912e9ffd176489c0e582cc0b55a29a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:08:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Setting mail';
+$lang['mail_settings_app_description'] = 'Mga Setting Mail app ay nagbibigay ng mga kinakailangang mga kasangkapan upang pamahalaan pangunahing pag-andar mail.';
+$lang['mail_settings_network_settings'] = 'Setting ng network';
+$lang['mail_settings_mail_domain'] = 'mail Domain';
