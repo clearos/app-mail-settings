f04e06c20d7b3c473d1e0e58bcef388b4a00253a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:00:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = '邮件设置';
+$lang['mail_settings_app_description'] = '邮件设置应用提供了必要的工具来管理核心的邮件功能。';
+$lang['mail_settings_network_settings'] = '网络设置';
+$lang['mail_settings_mail_domain'] = '邮件域';
