7f5dfc3db7d6b54b58cfa1a1eed7e69fdb2f958e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:00:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Zikhazikiko Mail';
+$lang['mail_settings_app_description'] = 'The app Mail Zikhazikiko amatipatsa zida zofunikira kugwiritsa ntchito pakati makalata magwiridwe.';
+$lang['mail_settings_network_settings'] = 'Zikhazikiko Network';
+$lang['mail_settings_mail_domain'] = 'Mail domeni';
