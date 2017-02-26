6385a6f6165706ff55a48b9cd33d42bb97418562
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:04:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'pasta iestatījumi';
+$lang['mail_settings_app_description'] = 'Pasta Settings app sniedz nepieciešamos instrumentus, lai pārvaldītu galveno pasta funkcionalitāti.';
+$lang['mail_settings_network_settings'] = 'Tīkla iestatījumi';
+$lang['mail_settings_mail_domain'] = 'pasts Domain';
