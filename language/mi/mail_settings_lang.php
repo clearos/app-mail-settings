8c705f27cf1ad4fa670071835b032ed8efb0278c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:04:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Tautuhinga Mail';
+$lang['mail_settings_app_description'] = 'Te taupānga Tautuhinga Mail whakarato te taputapu e tika ana ki te whakahaere i taumahinga mēra matua.';
+$lang['mail_settings_network_settings'] = 'Tautuhinga whatunga';
+$lang['mail_settings_mail_domain'] = 'Mail Domain';
