3b3c87a40d37bf7cf4cf76dd2e32875373c25bdb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:00:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Postavke pošte';
+$lang['mail_settings_app_description'] = 'Aplikacija Mail Settings osigurava potrebne alate za upravljanje temeljne funkcije pošte.';
+$lang['mail_settings_network_settings'] = 'Mrežne postavke';
+$lang['mail_settings_mail_domain'] = 'Domena e-pošte';
