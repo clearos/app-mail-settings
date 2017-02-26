5d52600846ac3468f97495910608fc82fad0de59
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:09:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Gosodiadau Mail';
+$lang['mail_settings_app_description'] = 'Mae&#39;r app Gosodiadau Mail yn darparu&#39;r offer angenrheidiol i reoli ymarferoldeb post craidd.';
+$lang['mail_settings_network_settings'] = 'Gosodiadau rhwydwaith';
+$lang['mail_settings_mail_domain'] = 'Parth Mail';
