d63825ac8628ae3d8954298310359fbbc035cd44
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:03:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Пошта параметрлері';
+$lang['mail_settings_app_description'] = 'Пошта параметрлері Қолданба негізгі пошта функционалдығын басқару үшін қажетті құралдар ұсынады.';
+$lang['mail_settings_network_settings'] = 'Желі параметрлері';
+$lang['mail_settings_mail_domain'] = 'Mail Домен';
