8aea99d97d997c28b97bfda49bd141be01af2e89
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:00:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'poŝto Agordoj';
+$lang['mail_settings_app_description'] = 'La Poŝto Fiksoj app provizas la necesajn ilojn por administri kerno poŝto funcionalidad.';
+$lang['mail_settings_network_settings'] = 'reto Agordoj';
+$lang['mail_settings_mail_domain'] = 'poŝto Domajno';
