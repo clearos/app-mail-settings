373c1bcaecfaa1a8e0b5be450005bc7908b95f84
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:58:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Mail ezarpenak';
+$lang['mail_settings_app_description'] = 'Mail ezarpenak aplikazio core mail funtzionalitate kudeatzeko beharrezko tresnak eskaintzen ditu.';
+$lang['mail_settings_network_settings'] = 'Sarearen ezarpenak';
+$lang['mail_settings_mail_domain'] = 'Mail Domain';
