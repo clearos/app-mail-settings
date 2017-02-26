8e1c1ecc4a1ddc14bea9619a249dffc9ac9b4c7e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:03:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'メール設定';
+$lang['mail_settings_app_description'] = 'メール設定アプリは、コアメール機能を管理するために必要なツールを提供します。';
+$lang['mail_settings_network_settings'] = 'ネットワーク設定';
+$lang['mail_settings_mail_domain'] = 'メールドメイン';
