e82c7368bcf018932c6d01c13a1ee00fe0a1bad6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:09:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'פּאָסט סעטטינגס';
+$lang['mail_settings_app_description'] = 'די פּאָסט סעטטינגס אַפּ גיט די נייטיק מכשירים צו פירן האַרץ פּאָסט פונקטיאָנאַליטי.';
+$lang['mail_settings_network_settings'] = 'נעטוואָרק סעטטינגס';
+$lang['mail_settings_mail_domain'] = 'מעיל דאָמאַין';
