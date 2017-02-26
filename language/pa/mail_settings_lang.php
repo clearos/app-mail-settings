15ff29c10a818b03a0a12b08f58638ed0185b56b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:06:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'ਮੇਲ ਸੈਟਿੰਗ';
+$lang['mail_settings_app_description'] = 'ਮੇਲ ਸੈਟਿੰਗ ਐਪ ਕੋਰ ਮੇਲ ਕਾਰਜਕੁਸ਼ਲਤਾ ਦਾ ਪਰਬੰਧਨ ਕਰਨ ਲਈ ਜ਼ਰੂਰੀ ਸੰਦ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['mail_settings_network_settings'] = 'ਨੈੱਟਵਰਕ ਸੈਟਿੰਗ';
+$lang['mail_settings_mail_domain'] = 'ਮੇਲ ਡੋਮੇਨ';
