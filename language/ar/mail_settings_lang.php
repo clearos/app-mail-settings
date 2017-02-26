2b6b25a8c25b2780204d2cf682ef6b09abcbd08d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:58:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'إعدادات البريد';
+$lang['mail_settings_app_description'] = 'يوفر تطبيق إعدادات البريد الأدوات اللازمة لإدارة وظائف البريد الأساسية.';
+$lang['mail_settings_network_settings'] = 'اعدادات الشبكة';
+$lang['mail_settings_mail_domain'] = 'المجال الإلكتروني';
