8c28fd4fad0e0f124b0a9ab1eea763d727f4315e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:06:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'تنظیمات ایمیل';
+$lang['mail_settings_app_description'] = 'برنامه تنظیمات ایمیل فراهم می کند ابزار لازم برای مدیریت قابلیت ایمیل هسته ای است.';
+$lang['mail_settings_network_settings'] = 'تنظیمات شبکه';
+$lang['mail_settings_mail_domain'] = 'ایمیل دامنه';
