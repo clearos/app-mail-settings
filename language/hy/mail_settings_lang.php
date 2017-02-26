5a894db78ff4f3cd6eaf7293b4b202c15861d3cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:58:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Փոստ կարգավորումներ';
+$lang['mail_settings_app_description'] = 'The Փոստ Կառավարում ծրագիրը ապահովում է անհրաժեշտ գործիքներ կառավարել հիմնական փոստի ֆունկցիոնալությունը:';
+$lang['mail_settings_network_settings'] = 'Ցանցային կարգավորումներ';
+$lang['mail_settings_mail_domain'] = 'Փոստ Դոմեն';
