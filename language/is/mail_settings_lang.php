3886502d4cda9aa68f561b346a87e17a434693aa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:02:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Póststillingar';
+$lang['mail_settings_app_description'] = 'The Póststillingar app veitir nauðsynlegar verkfæri til að stjórna algerlega tölvupóstinn.';
+$lang['mail_settings_network_settings'] = 'netstillingar';
+$lang['mail_settings_mail_domain'] = 'Tölvupóstlén';
