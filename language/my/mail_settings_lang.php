bc3f6f573f844545401a4cf691e2b3dece2d2536
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:59:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'မေးလ်က Settings';
+$lang['mail_settings_app_description'] = 'အဆိုပါမေးလ်က Settings app ကို core ကိုမေးလ်လုပ်ဆောင်နိုင်စွမ်းကိုစီမံခန့်ခွဲရန်လိုအပ်သော tools တွေကိုပေးစွမ်းသည်။';
+$lang['mail_settings_network_settings'] = 'Network ကိုချိန်ညှိ';
+$lang['mail_settings_mail_domain'] = 'မေးလ်ဒိုမိန်း';
