f98c2c8957fa277a8df57d6f15598afa67bbfd3b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:07:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Setélan surat';
+$lang['mail_settings_app_description'] = 'Cijeungjing Setélan aplikasi nyadiakeun pakakas perlu ngatur pungsi surélék inti.';
+$lang['mail_settings_network_settings'] = 'Setélan jaringan';
+$lang['mail_settings_mail_domain'] = 'surat Domain';
