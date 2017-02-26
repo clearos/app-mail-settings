59a352c1fb25556646651f46e4115d0b9293d1a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:04:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Cursuales';
+$lang['mail_settings_app_description'] = 'A app Cursuales suggero tools necessaria ad regendum Core mail functionality.';
+$lang['mail_settings_network_settings'] = 'Network Occasus';
+$lang['mail_settings_mail_domain'] = 'Domain Mail';
