60ff7cc43a5e42a9d30ec76037306b2e929938ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:04:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'paštas Nustatymai';
+$lang['mail_settings_app_description'] = 'Pašto nustatymus programa suteikia reikiamas priemones valdyti pagrindinę pašto funkcionalumą.';
+$lang['mail_settings_network_settings'] = 'Ryšio nustatymai';
+$lang['mail_settings_mail_domain'] = 'paštas Domenų';
