dcc00dfd33199767cfe8bc1c71942a8816b7b630
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:04:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'ການຕັ້ງຄ່າ mail';
+$lang['mail_settings_app_description'] = 'app ຕັ້ງ Mail ມີເຄື່ອງມືທີ່ຈໍາເປັນໃນການຄຸ້ມຄອງການເຮັດວຽກ mail core.';
+$lang['mail_settings_network_settings'] = 'ການຕັ້ງຄ່າເຄືອຂ່າຍ';
+$lang['mail_settings_mail_domain'] = 'Domain mail';
