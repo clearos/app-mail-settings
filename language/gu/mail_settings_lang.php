f986f44edb7ae0fee928f781b59d1f95aa70ccef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:01:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'મેલ સેટિંગ્સ';
+$lang['mail_settings_app_description'] = 'મેલ સેટિંગ્સ એપ્લિકેશન કોર મેલ વિધેય વ્યવસ્થા કરવા માટે જરૂરી સાધનો પૂરા પાડે છે.';
+$lang['mail_settings_network_settings'] = 'નેટવર્ક સેટિંગ્સ';
+$lang['mail_settings_mail_domain'] = 'મેઈલ ડોમેઈન';
