074debe7803dd7ebaa5f9a02150dea2588439d90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:02:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'मेल सेटिंग्स';
+$lang['mail_settings_app_description'] = 'मेल सेटिंग्स एप्लिकेशन कोर मेल कार्यक्षमता का प्रबंधन करने के लिए आवश्यक उपकरण प्रदान करता है।';
+$lang['mail_settings_network_settings'] = 'नेटवर्क सेटिंग';
+$lang['mail_settings_mail_domain'] = 'मेल डोमेन';
