13eb1aaa2e78144e7badb43389f8c08d784afcd3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:59:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'налады пошты';
+$lang['mail_settings_app_description'] = 'Дадатак Налады пошты прадастаўляе неабходныя прылады для кіравання асноўныя функцыянальныя магчымасці электроннай пошты.';
+$lang['mail_settings_network_settings'] = 'сеткавыя налады';
+$lang['mail_settings_mail_domain'] = 'паштовы дамен';
