fb13bdfb7446b7d779436b2a9ddc1472c2f28941
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:06:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Configurações de Mensagens';
+$lang['mail_settings_app_description'] = 'O aplicativo Configurações de Mensagens fornece as ferramentas necessárias para gerenciar as funcionalidades de correio núcleo.';
+$lang['mail_settings_network_settings'] = 'Configurações de rede';
+$lang['mail_settings_mail_domain'] = 'Domínio de correio';
