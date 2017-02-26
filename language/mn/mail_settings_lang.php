a55660a3afc7df034fd11a51546975c1fc3bfd65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:05:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'Захидал Тохиргоо';
+$lang['mail_settings_app_description'] = 'Захидал Тохиргоо апп гол мэйл ажиллагааг зохицуулах шаардлагатай багаж хэрэгсэл байдаг.';
+$lang['mail_settings_network_settings'] = 'Сүлжээний Тохиргоо';
+$lang['mail_settings_mail_domain'] = 'Захидал Домэйн';
