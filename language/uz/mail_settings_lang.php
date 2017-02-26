1c8600249486278f3bc596f1e7d8ebbb607b202a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:09:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'mail Settings';
+$lang['mail_settings_app_description'] = 'Mail Sozlamalar ilova yadroli mail funksiyalarini boshqarish uchun zarur vositalarni beradi.';
+$lang['mail_settings_network_settings'] = 'tarmoq Sozlamalar';
+$lang['mail_settings_mail_domain'] = 'mail Domen';
