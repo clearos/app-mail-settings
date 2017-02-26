036944044cfc0c5d4c77825c296ef81149f1089b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:06:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'postinnstillinger';
+$lang['mail_settings_app_description'] = 'Den postinnstillinger app gir de nødvendige verktøy for å administrere kjernen postfunksjonalitet.';
+$lang['mail_settings_network_settings'] = 'Nettverksinnstillinger';
+$lang['mail_settings_mail_domain'] = 'mail Domain';
