c60d18f0301fdaac28d378f897326b5b3c0d6b1c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:10:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/mail_settings_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_settings_app_name'] = 'mail Eto';
+$lang['mail_settings_app_description'] = 'The Mail Eto app pese awọn pataki irinṣẹ lati ṣakoso awọn mojuto mail iṣẹ.';
+$lang['mail_settings_network_settings'] = 'Network Eto';
+$lang['mail_settings_mail_domain'] = 'mail ase';
